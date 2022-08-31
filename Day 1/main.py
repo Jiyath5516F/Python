83b09eeb7e8d8895f16d83f9c08cb9379519eadd
Mohamed Jiyath Khan Ibn Ibramsha <75976630+Jiyath5516F@users.noreply.github.com>
2022-08-31 14:09:38 +0100

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,74 @@
+#           Variable
+#
+# first_name = "jiyath"
+# last_name = "Khan"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+
+# age = 18
+# age = age + 1
+# print(type(age))
+# print("Your age is: " + str(age))
+
+# height = 205.50
+# print(height)
+# print(type(height))
+# print("Your height is: " + str(height) + " cm")
+
+# human = True
+# print("Are You a Human: " + str(human))
+# print(type(human))
+
+#       Multiple assignment
+
+# name = "jiyath"
+# age = 19
+# attractive = True
+
+# name, age, attractive = "Jiyath", 19, True
+#
+# print(name)
+# print(age)
+# print(attractive)
+#
+# a = b = c = d = 30
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+#   String Methods in python
+# name = "JiyathKahn"
+# print(name.find("n"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("a"))
+# print(name.replace("a","A"))
+# print(name*5)
+
+
+
+# Type casting : convert the data type of a value to another data type
+
+# x = 1
+# y = 2.4
+# z = "5"
+# z = int(z)
+# x = float(x)
+# print(x)
+# print(y)
+# print(z*4)
+
+
+# User Input
+name = input("What is your name: ")
+age = int(input("How old are you : "))
+height = float(input("What your height: "))
+height = str(height)
+print("Hello " + name)
+print("You are " + str(age) + " years old")
+print("your height is : " + height)
